20c2936b6b7127596c1cc167e927c9a26ee3bb8d
一荤一素 <wenbo_Y@126.com>
2021-03-15 10:36:28 +0800

    Update RedisDelayQueue.java

--- src/main/java/org/ywb/redisdelayqueue/queue/RedisDelayQueue.java
+++ src/main/java/org/ywb/redisdelayqueue/queue/RedisDelayQueue.java
@@ -24,34 +24,11 @@ public class RedisDelayQueue {
     /**
      * 向延迟队列中投放任务
      *
-     * @param keyGen 任务队列名称
-     * @param task   任务
-     * @param delay  延迟时长，单位毫秒
-     */
-    public void delay(Supplier<String> keyGen, String topic, Object task, Long delay) {
-        delayQueuePolling.delay(keyGen, topic, task, delay);
-    }
-
-    /**
-     * 向延迟队列中投放任务
-     *
-     * @param keyGen   任务队列名称
-     * @param task     任务
-     * @param delay    延迟时长
-     * @param timeUnit 延迟时间单位
-     */
-    public void delay(Supplier<String> keyGen, String topic, Object task, Long delay, TimeUnit timeUnit) {
-        delay(keyGen, topic, task, timeUnit.toMillis(delay));
-    }
-
-    /**
-     * 向延迟队列中投放任务
-     *
      * @param task  任务
      * @param delay 延迟时间，单位毫秒
      */
-    public void delay(String topic, Object task, Long delay) {
-        delay(() -> DateTimeFormatter.ofPattern("yyyyMMddHH").format(LocalDateTime.now()), topic, task, delay);
+    private void delay(String topic, Object task, Long delay) {
+        delayQueuePolling.delay(() -> DateTimeFormatter.ofPattern("yyyyMMddHH").format(LocalDateTime.now()), topic, task, delay);
     }
 
     /**
@@ -63,5 +40,63 @@ public class RedisDelayQueue {
      */
     public void delay(String topic, Object task, Long delay, TimeUnit timeUnit) {
         delay(topic, task, timeUnit.toMillis(delay));
+    }
+
+    /**
+     * 向延迟队列中投放任务
+     *
+     * @param topic    队列的topic
+     * @param supplier 延迟任务制造函数
+     * @param delay    延迟时间
+     * @param timeUnit 延迟时间单位
+     * @since 1.0.1
+     */
+    public void delay(String topic, Supplier<Object> supplier, Long delay, TimeUnit timeUnit) {
+        delay(topic, supplier.get(), delay, timeUnit);
+    }
+
+    /**
+     * 向延迟队列中投放任务
+     *
+     * @param topic    队列的topic
+     * @param supplier 延迟任务制造函数
+     * @param endTime  期望获取到延迟任务的时间
+     * @since 1.0.1
+     */
+    public void delay(String topic, Supplier<Object> supplier, LocalDateTime endTime) {
+        delay(topic, supplier.get(), getDelayTime(endTime), TimeUnit.MILLISECONDS);
+    }
+
+    /**
+     * 向延迟队列中投放任务
+     *
+     * @param topic    队列的topic
+     * @param function 延迟任务制造函数
+     * @param t        延迟任务制造函数入参
+     * @param delay    延迟时间
+     * @param timeUnit 延迟时间单位
+     * @since 1.0.1
+     */
+    public <T> void delay(String topic, Function<T, Object> function, T t, Long delay, TimeUnit timeUnit) {
+        delay(topic, function.apply(t), delay, timeUnit);
+    }
+
+    /**
+     * 向延迟队列中投放任务
+     *
+     * @param topic    队列的topic
+     * @param function 延迟任务制造函数
+     * @param t        延迟任务制造函数入参
+     * @param endTime  期望获取到延迟任务的时间
+     * @since 1.0.1
+     */
+    public <T> void delay(String topic, Function<T, Object> function, T t, LocalDateTime endTime) {
+        delay(topic, function.apply(t), getDelayTime(endTime), TimeUnit.MILLISECONDS);
+    }
+
+    private long getDelayTime(LocalDateTime endTime) {
+        final long endTimeMilli = endTime.toInstant(ZoneOffset.of("+8")).toEpochMilli();
+        final long currentMilli = LocalDateTime.now().toInstant(ZoneOffset.of("+8")).toEpochMilli();
+        return currentMilli - endTimeMilli;
     }
 }
